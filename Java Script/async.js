12d95a54a2b62d31bc5bb182845ea02f7fcc9411
madhav5285 <sharmamadhav5285@gmail.com>
2026-08-25 09:27:41 +0530

    sync code

---
+++ Java Script/async.js
@@ -0,0 +1,10 @@
+console.log("task 1");
+console.log("task 2");
+setTimeout(()=>{
+    console.log("task 3");
+},200);
+setTimeout(()=>{
+    console.log("task 4");
+},2000);
+
+console.log("task 5");
---
+++ Java Script/sync.js
@@ -0,0 +1,7 @@
+console.log("task1");
+for(let i=1;i<=10000;i++){
+    console.log(i);
+}
+console.log("task2");
+console.log("task three");
+console.log("hi  ABES");
